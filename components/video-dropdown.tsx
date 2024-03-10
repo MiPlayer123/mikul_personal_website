30b91de96ae5ee91ef68f89759525981be47e8d5
Mikul Saravanan <mikulsaravanan@gmail.com>
2024-03-10 15:34:10 -0400

    fixed video button

--- components/video-dropdown.tsx
+++ components/video-dropdown.tsx
@@ -10,16 +10,23 @@ export default function VideoDropdown() {
   return (
     <section className="mb-20 sm:mb-30">
       <div className="accordion items-center justify-center gap-2 text-lg text-black">
-        <button className="accordion-button justify-center bg-white borderBlack p-4 text-black hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer dark:bg-white/20 dark:text-white/80" onClick={toggleOpen}>
+        <div className="flex justify-center">
+        <button
+          className="accordion-button bg-white borderBlack p-4 text-black hover:text-gray-950 flex items-center gap-2 rounded-full transition cursor-pointer dark:bg-white/20 dark:text-white/80"
+          onClick={toggleOpen}
+          style={{ transition: 'transform 0.25s', transformOrigin: 'center' }}
+        >
           {isOpen ? 'Hide Video' : 'Show 2023 Maker Profile Video'}
         </button>
+        </div>
 
         {isOpen && (
-          <div className="mt-4">
-            <br className='mb-10'/>
+          <div className="mt-4 w-full">
             <div className="aspect-w-16 aspect-h-9">
               <iframe
-                className="w-full h-full"
+                width="640"
+                height="360"
+                className="mt-4 rounded-lg shadow-lg border-2 dark:border-white/20"
                 src="https://www.youtube.com/embed/SejmLAb3-iA"
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
